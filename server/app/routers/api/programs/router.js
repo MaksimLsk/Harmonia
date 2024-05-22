1fece73607fca54790cb310bfef792331871082b
Maksim Losjuk <m.lysuk@gmail.com>
2024-05-22 15:35:22 +0200

    harmonia Routing, quete 03

--- server/app/routers/api/programs/router.js
+++ server/app/routers/api/programs/router.js
@@ -3,22 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 
-const { sayWelcome } = require("../../controllers/sayActions");
+const { browse } = require("../../../controllers/programActions");
 
-router.get("/", sayWelcome);
+router.get("/", browse);
 
 
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
 
-const itemsRouter = require("./items/router");
 
-router.use("/items", itemsRouter);
-
-const programActionsRouter = require("./programs/router");
-
-router.use("/programs", programActionsRouter);
 
 /* ************************************************************************* */
 
